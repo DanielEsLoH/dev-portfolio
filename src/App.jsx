cd76d3fc1c7b1213d63eeb2e6bd1d7626e666107
Daniel E. Londoño <daniel.esloh@gmail.com>
2025-07-24 11:54:19 -0500

    The ThemeContext.jsx file is incorporated to add the dark mode feature to the app. The Navbar.jsx component is also created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>App</div>
-  )
-}
+    <ThemeProvider>
+      <div>
+        <Navbar />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App
